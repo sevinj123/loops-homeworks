80b5c9d39a86f86463f55c53561a0748ce43e420
Sevinc <70HF4C3@code.edu.az>
2023-04-14 22:45:56 +0400

    added file

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+//1.Ededin reqemleri cemini tapin
+
+// var num = prompt("eded daxil edin");
+// sum = 0;
+// var teklik=num%10;
+// while (num > 0) {
+// teklik=num%10;
+//   teklik = num % 10;
+//    num = (num - teklik) / 10;
+//    sum = sum + teklik
+// }
+// alert(sum);
+
+
+
+
+ 
+ //2. Daxil olunan müsbət tam ədədin sadə və ya mürəkkəb olduğunu tapın
+ 
+
+var num = number(prompt("musbet ededi daxil edin"));
+if (num === 1)
+ {
+    console.log("1 ne sade ne de murekkeb ededdir");
+}
+else 
+if (num > 1)
+ {
+    for (var i = 2; i < number; i++)
+     { 
+      if (num % i == 0)
+    } 
+       else 
+       { 
+         console.log(`${num} sade reqem deyil`);
+       } 
+      }
+     
