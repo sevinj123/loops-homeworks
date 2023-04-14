eefa1bbbe8fcb57053436904f8e6097a45a451cb
Sevinc <70HF4C3@code.edu.az>
2023-04-14 23:15:29 +0400

    first comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,21 +19,38 @@
  //2. Daxil olunan müsbət tam ədədin sadə və ya mürəkkəb olduğunu tapın
  
 
-var num = number(prompt("musbet ededi daxil edin"));
-if (num === 1)
- {
-    console.log("1 ne sade ne de murekkeb ededdir");
-}
-else 
-if (num > 1)
- {
-    for (var i = 2; i < number; i++)
-     { 
-      if (num % i == 0)
-    } 
-       else 
-       { 
-         console.log(`${num} sade reqem deyil`);
-       } 
-      }
+ var num =prompt("musbet ededi daxil edin");
+ 
+ for (var i = 2; i < number; i++){
+    if(num%2==0){
+        alert("murekkebdir")
+    }
+    else{
+        alert("sadedir")
+    }
+    break;
+ }
+ 
+
+
+
+
+//  //ve ya 
+// var num = number(prompt("musbet ededi daxil edin"));
+// if (num === 1)
+//  {
+//     console.log("1 ne sade ne de murekkeb ededdir");
+// }
+// else 
+// if (num > 1)
+//  {
+//     for (var i = 2; i < number; i++)
+//      { 
+//       if (num % i == 0)
+//     } 
+//        else 
+//        { 
+//          console.log(`${num} sade reqem deyil`);
+//        } 
+//       }
      
